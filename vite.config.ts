af479582bf20836128a9c1061efb220ee408c310
unknown <gaplee@foxmail.com>
2026-02-06 19:34:13 +0800

    upd1

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    open: true,
+  define: {
+    'process.env': process.env
   }
 });
